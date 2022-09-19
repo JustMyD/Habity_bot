eee5af3b2fe33215028342f77198e5f7de4d7e2d
JustMyD <commonqued@gmail.com>
2022-09-19 21:16:48 +0300

    Second push

--- init_bot.py
+++ init_bot.py
@@ -1,7 +1,9 @@
 import os
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import Text
 
+from bot_messages import GREETING_MSG, USER_GOAL_MSG
 
 bot = Bot(token=os.getenv('API_TOKEN'))
 dp = Dispatcher(bot)
@@ -9,4 +11,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def greetings(message: types.Message):
-    await message.answer(text=message.text)
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.add(types.KeyboardButton(text='Отправить цель'))
+    await message.answer(text=GREETING_MSG, reply_markup=keyboard)
+
+
+@dp.message_handler(Text('Отправить цель'))
+async def get_user_goal(message: types.Message):
+    await message.answer(text=USER_GOAL_MSG)
