2399824cc9cefa49d76e9486e1dc9d97c8b5489d
www@294081 <www@294081.simplecloud.ru>
2022-09-28 19:53:35 +0300

    server commit

--- src/bot/habity_bot_mailing.sh
+++ src/bot/habity_bot_mailing.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#cd /home/www/Bot_projects/Habity_bot/src/bot/
-#source ../../env/bin/activate
-#python3 send_daily_message.py
-echo 'Hello world'
+cd /home/www/Bot_projects/Habity_bot/src/bot/
+source ../../env/bin/activate
+python3 send_daily_message.py
